ae84ba6224c6c358dcd799ad614396fa2d21302d
Ilya Ablin <ablin66@mail.ru>
2020-05-25 22:20:01 +0500

    проветка title и закрытие браузера

--- src/test/java/ru/deliveryClub/FirstTest.java
+++ src/test/java/ru/deliveryClub/FirstTest.java
@@ -1,13 +1,20 @@
 package ru.deliveryClub;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
     @Test
     public void firstTest() {
-        System.setProperty("webdriver.chrome.driver",".\\drivers\\chromedriver81.exe");
+        System.setProperty("webdriver.chrome.driver",".\\drivers\\chromedriver83.exe");
         ChromeDriver driver = new ChromeDriver();
+
         driver.get("https://www.delivery-club.ru/ekb");
+
+        String title = driver.getTitle();
+        Assert.assertTrue(title.equals("Доставка еды из ресторанов Екатеринбурга от 45 минут! Delivery Club"));
+
+        driver.quit();
     }
 }
