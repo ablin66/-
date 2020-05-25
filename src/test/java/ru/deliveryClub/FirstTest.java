4563ac825706114c5e5b58ca3bf69de21f9ee3a6
georg <rusanovgeorgy@gmail.com>
2020-05-25 13:46:44 +0300

    поправил

--- src/test/java/ru/deliveryClub/FirstTest.java
+++ src/test/java/ru/deliveryClub/FirstTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FirstTest {
     @Test
     public void firstTest() {
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\Downloads\\chromedriver_win32111\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver",".\\drivers\\chromedriver81.exe");
         ChromeDriver driver = new ChromeDriver();
         driver.get("https://www.delivery-club.ru/ekb");
     }
